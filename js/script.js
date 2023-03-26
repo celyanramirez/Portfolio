843dd88d398d3f8e35f05ebfd76c37914308aabd
celyanramirez <celyan.ramirez@gmail.com>
2023-03-26 22:41:07 +0200

    Modification nav

--- js/script.js
+++ js/script.js
@@ -9,10 +9,26 @@ function onScroll() {
   // déplace l'image de fond et les textes selon la position de scroll
   backgroundImg.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
   textElements.forEach((el) => {
-    el.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+    el.style.transform = `translateY(${scrollPosition * 0.5}px)`;
   });
+}
+
+      
+function reveal(){
+  let reveals = document.querySelectorAll('.reveal');
+
+  for(let i=0; i<reveals.length; i++){
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add('active');
+    }
+  }
 }
 
 
 // attache la fonction de scroll à l'événement "scroll"
 window.addEventListener('scroll', onScroll);
+window.addEventListener('scroll', reveal);
